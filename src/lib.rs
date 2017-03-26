df6292d5ad438d320abf539d21075755e2d5b9e2
Lucille Blumire <llblumire@gmail.com>
2017-03-26 14:15:35 +0100

    Multithreaded, cleaned up type gore

--- src/lib.rs
+++ src/lib.rs
@@ -1,104 +1,101 @@
-extern crate rand;
+extern crate rayon;
 extern crate typemap;
 
-use rand::Rand;
-use rand::Rng;
-use rand::ThreadRng;
+use rayon::prelude::*;
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::Weak;
 use typemap::ShareMap;
 
+pub type PropertyMap = ShareMap;
+pub type GlobalState = Arc<Mutex<ShareMap>>;
+pub type Map<K, V> = BTreeMap<K, V>;
+pub type Set<T> = BTreeSet<T>;
+
 pub trait Entity {
-    fn property_map(&mut self) -> &mut ShareMap;
-    fn run_self(&mut self) -> bool;
+    fn property_map(&mut self) -> &mut PropertyMap;
 }
+pub type WeakEntity = Weak<Mutex<Entity + Send>>;
+pub type StrongEntity = Arc<Mutex<Entity + Send>>;
 
 pub trait Controller {
-    fn run_on(&mut self, entity: &mut ShareMap, global_state: &mut ShareMap);
+    fn control(&mut self, entity: StrongEntity, global_state: GlobalState);
 }
+pub type StrongController = (Arc<Mutex<Box<Controller + Send>>>, Arc<Mutex<Vec<WeakEntity>>>);
 
-pub struct XGEngine<I = u64, R = ThreadRng> {
-    pub entities: BTreeMap<I, Arc<Mutex<Entity>>>,
-    pub controllers: BTreeMap<I, (Arc<Mutex<Controller>>, BTreeMap<I, Weak<Mutex<Entity>>>)>,
-    pub rng_source: R,
-    pub global_state: ShareMap
+
+pub struct XGEngine {
+    pub entities: Map<u64, StrongEntity>,
+    pub controllers: Map<u64, StrongController>,
+    pub global_state: GlobalState,
+    used_entity_keys: Set<u64>,
+    next_entity_key: u64,
+    used_controller_keys: Set<u64>,
+    next_controller_key: u64,
 }
-
 impl XGEngine {
     pub fn new() -> XGEngine {
-        XGEngine::from_rng(rand::thread_rng())
-    }
-}
-
-impl<I, R> XGEngine<I, R>
-    where I: Rand + Ord + Clone,
-          R: Rng
-{
-    pub fn from_rng(r: R) -> XGEngine<I, R> {
         XGEngine {
-            entities: BTreeMap::new(),
-            controllers: BTreeMap::new(),
-            rng_source: r,
-            global_state: ShareMap::custom(),
+            entities: Map::new(),
+            controllers: Map::new(),
+            global_state: Arc::new(Mutex::new(ShareMap::custom())),
+            used_entity_keys: Set::new(),
+            next_entity_key: 0,
+            used_controller_keys: Set::new(),
+            next_controller_key: 0,
         }
     }
 
-    pub fn entity(&mut self, entity: Arc<Mutex<Entity>>) -> I {
-        let mut key = self.rng_source.gen::<I>();
-        loop {
-            if self.entities.contains_key(&key) {
-                key = self.rng_source.gen::<I>();
-            } else {
-                self.entities.insert(key.clone(), entity);
-                return key;
-            }
-        }
+    pub fn entity(&mut self, entity: StrongEntity) -> u64 {
+        let key = keygen(&self.used_entity_keys, &mut self.next_entity_key);
+        self.entities.insert(key, entity);
+        key
     }
 
-    pub fn controller(&mut self, controller: Arc<Mutex<Controller>>) -> I {
-        let mut key = self.rng_source.gen::<I>();
-        loop {
-            if self.controllers.contains_key(&key) {
-                key = self.rng_source.gen::<I>();
-            } else {
-                self.controllers.insert(key.clone(), (controller, BTreeMap::new()));
-                return key;
-            }
-        }
+    pub fn controller(&mut self, controller: StrongController) -> u64 {
+        let key = keygen(&self.used_controller_keys, &mut self.next_controller_key);
+        self.controllers.insert(key, controller);
+        key
     }
 
-    pub fn register(&mut self, controller_id: I, entity_id: I) -> Result<(), RegisterError> {
-        match (self.controllers.get_mut(&controller_id), self.entities.get(&entity_id)) {
-            (Some(&mut (_, ref mut registrant)), Some(ref entity)) => {
-                registrant.insert(entity_id.clone(), Arc::downgrade(entity));
-                Ok(())  
+    pub fn register(&mut self, controller: StrongController, entity: WeakEntity) {
+        controller.1.lock().unwrap().push(entity);
+    }
+
+    pub fn register_by_id(&mut self, controller_id: u64, entity_id: u64) -> bool {
+        match (self.controllers.get(&controller_id).cloned(), self.entities.get(&entity_id).cloned()) {
+            (Some(controller), Some(entity)) => {
+                self.register(controller, StrongEntity::downgrade(&entity));
+                true
             },
-            (Some(_), None) => Err(RegisterError::NoEntityID),
-            (None, Some(_)) => Err(RegisterError::NoControllerID),
-            (None, None) => Err(RegisterError::NoControllerOrEntityID),
+            _ => false,
         }
     }
 
     pub fn run_all(&mut self) {
-        for (_, &mut (ref mut controller, ref mut registrant)) in self.controllers.iter_mut() {
-            for (_, entity) in registrant.iter() {
+        self.controllers.par_iter().for_each(|(_, &(ref controller, ref registrant))|{
+            let registrant = registrant.lock().unwrap();
+            registrant.par_iter().for_each(|entity|{
+                let mut controller = controller.lock().unwrap();
                 if let Some(entity) = entity.upgrade() {
-                    match (entity.lock(), controller.lock()) {
-                        (Ok(mut entity), Ok(mut controller)) => {
-                            controller.run_on(entity.property_map(), &mut self.global_state);
-                        }
-                        _ => {}
-                    }
+                    controller.control(entity, self.global_state.clone());
                 }
-            }
-        }
+            });
+        });
     }
 }
 
-pub enum RegisterError {
-    NoControllerID,
-    NoEntityID,
-    NoControllerOrEntityID,
+fn keygen(used: &Set<u64>, next: &mut u64) -> u64 {
+    let mut key;
+    loop {
+        key = *next;
+        if used.contains(&key) {
+            *next += 1;
+        } else {
+            break;
+        }
+    }
+    key
 }
